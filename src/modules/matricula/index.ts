2f807fe62d8ba29b59e4d6ced2542cff5f30674d
davidvasquez02 <70860023+davidvasquez02@users.noreply.github.com>
2026-09-09 02:39:41 -0500

    Revert "Habilitar notificación de apertura de matrícula"

--- src/modules/matricula/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  consultarPeriodoMatriculaVigente,
-  notificarAperturaMatricula,
-} from './api/matriculaNotificacionService'
-export { NotificacionAperturaMatricula } from './components/NotificacionAperturaMatricula'
-export type { PeriodoAcademico, PeriodoAcademicoFecha, TipoTramite } from './types'
