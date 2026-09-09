ea13f5d5a4244c992311ab2e36ba9ffeb88988d5
davidvasquez02 <70860023+davidvasquez02@users.noreply.github.com>
2026-09-09 01:21:58 -0500

    Revert "Agregar gestión de estado del estudiante"

--- src/modules/estudiantes/index.ts
+++
@@ -1,3 +0,0 @@
-export { actualizarEstadoEstudiante } from './api/estudianteEstadoService'
-export { EstudianteEstadoActions } from './components/EstudianteEstadoActions'
-export type { EstadoEstudiante, EstudianteResumen } from './types'
